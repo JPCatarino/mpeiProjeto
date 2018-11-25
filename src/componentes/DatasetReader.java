e31009ca30d77098670033c0576ec8eaf555e490
JPCatarino <jorge.catarino@ua.pt>
2018-11-25 22:37:44 +0000

    Adicionado esboço classe Match, adicionado pequeno teste

---
+++ src/componentes/DatasetReader.java
@@ -0,0 +1,40 @@
+package componentes;
+
+import casasDeApostas.Match;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+
+public class DatasetReader {
+    private static String csvPath = "src\\componentes\\dataset\\closing_odds.csv";
+    private static String csvSplit = ",";
+
+
+    public static Match[] readMatches(){
+        BufferedReader nReader;
+        String line;
+        Match[] readMatches = new Match[500000];
+        int i = 0;
+        try{
+            nReader = new BufferedReader(new FileReader(csvPath));
+            nReader.readLine();
+            while((line = nReader.readLine()) != null){
+                String[] match = line.split(csvSplit);
+                readMatches[i] = new Match(match[3],Integer.parseInt(match[4]), match[5], Integer.parseInt(match[6]));
+                i++;
+            }
+            nReader.close();
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return readMatches;
+    }
+
+}
---
+++ src/testes/testCBF3.java
@@ -0,0 +1,25 @@
+package testes;
+
+
+import casasDeApostas.Match;
+import componentes.CountingBloomFilter;
+import componentes.DatasetReader;
+
+public class testCBF3 {
+
+    public static void main(String[] args) {
+        Match[] matches = DatasetReader.readMatches();
+        CountingBloomFilter<Match> cbf = new CountingBloomFilter<Match>(500000 * 15, 32, 500000 * 15 / 500000);
+        System.out.println("Inserting Elems");
+        for(int i = 0; i < matches.length; i++){
+            cbf.insertElem(matches[i]);
+        }
+        System.out.println("done");
+
+        if(cbf.isMember(matches[1]))
+            System.out.println(cbf.count(matches[1]));
+            System.out.println("cool");
+    }
+
+
+}
